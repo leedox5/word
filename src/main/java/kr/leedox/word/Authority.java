850c2d6d163569d083df5499b125a3c3a4e8cc75
LEEDOX <leedox@naver.com>
2023-10-11 11:17:27 +0900

    add user entity

---
+++ src/main/java/kr/leedox/word/Authority.java
@@ -0,0 +1,19 @@
+package kr.leedox.word;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+    @Id
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/kr/leedox/word/User.java
@@ -0,0 +1,31 @@
+package kr.leedox.word;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @JsonIgnore
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_id", referencedColumnName = "id")}
+    )
+    private Set<Authority> authorities;
+}
